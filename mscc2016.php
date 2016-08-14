05adb88866d8a7ed67b5c334ddf8f322bcafa57b
vaibhavagarwal220 <vaibhavagarwal220@gmail.com>
2016-08-14 23:35:02 +0530

    Added 'content/' for bootstrap functionality and similarly added 'design' and 'js' for css and js purposes. Added all the base image files. Added the msc2016 and 2015 batches calendar. Modified the fourth.php and design.php with added changes of the functionality of list menu. Other changes to be done afterwards. Concentrating on MDL.

---
+++ mscc2016.php
@@ -0,0 +1,37 @@
+
+<html>
+  <head>
+  <meta charset="utf-8">
+  
+  <script type="text/javascript" src=js/jquery.min.js></script>
+  <script type="text/javascript" src=content/js/bootstrap.min.js></script>
+  <link rel="stylesheet" type="text/css" href="content/css/bootstrap.min.css">
+  <script type="text/javascript" src=js/send.js></script>
+  <link rel="stylesheet" type="text/css" href="design.css">
+  <style type="text/css">#clist {font-size:20px;}
+
+         option {padding:5px;text-align:center;}
+         tr{font-size: 20px;}
+         .jumbotron{background-color:#00F4F4;color:;text-align: center;font-family:monospace;background-image:radial-gradient(#00F4F4,#119191);}
+         .small{width:15%;height:15%;}
+         .tt{width:70%;height:70%;}
+         .container1{background-image:url(mscc2016.png);width:100%;height:100%;background-repeat:no-repeat;margin-left:250px;}
+         .container2{background-image:url(mscc.png);width:100%;height:100%;background-repeat:no-repeat;margin-left:250px;}
+         .container3{background-image:url(mscm2016.png);width:100%;height:100%;background-repeat:no-repeat;margin-left:220px;}
+         </style>
+
+  </head>
+  <body>
+  
+  	<div class=jumbotron><h2><img src=logo_iit1.png class=small> M.Sc.</h2>
+  	<h3>M.Sc.(Chemistry) 2016,2015,M.Sc.(Applied Mathematics) 2016 Batch</h3>
+  	</div>
+  	<div class=container1>
+  </div>
+  <div class=container2>
+  </div>
+  <div class=container3>
+  </div>
+
+  </body>
+</html>
