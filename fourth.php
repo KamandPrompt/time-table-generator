8932a39c9e5dab312ed16572b3de44e34ce4680a
vaibhavagarwal220 <vaibhavagarwal220@gmail.com>
2016-08-18 04:07:57 +0530

    Add files via upload
    Added MDL and Main Page

--- fourth.php
+++ fourth.php
@@ -1,5 +1,7 @@
 
 <?php 
+//I4=H4
+//I3=H3
 $time = array
   (
   "","8:00-9:00 AM","9:00-10:00 AM","10:00-11:00 AM","11:00-12:00 PM","12:00-1:00PM","1:00-2:00 PM","2:00-3:00 PM","3:00-4:00 PM","4:00-5:00 PM","5:00-6:00 PM","6:00-7:00 PM"
@@ -7,24 +9,25 @@ $time = array
  $days =array( "Monday","Tuesday","Wednesday","Thursday","Friday");
 $ftime = array
   (
-  array("<div class=\"A3 A4\"></div>","<div class=\"B3 B4\"></div>","<div class=\"C3 C4\"></div>","<div class=\"D3 D4\"></div>","L","<div class=\"G3 G4\"></div>","<div class=\"H3 H4\"></div><div class=L1></div>","<div class=L1></div>","<div class=L1></div>","",""),
+  array("<div class=\"A3 A4\"></div>","<div class=\"B3 B4\"></div>","<div class=\"C3 C4\"></div>","<div class=\"D3 D4\"></div>","L","<div class=\"G3 G4\"></div>","<div class=\"I3 I4\"></div><div class=L1></div>","<div class=L1></div>","<div class=L1></div>","",""),
   
-  array("<div class=\"E3 E4\"></div>","<div class=\"F3 F4\"></div>","<div class=\"G3 G4\"></div>","<div class=\"H3 H4\"></div>","U","<div class=A4></div>","<div class=L2></div><div class=C4></div>","<div class=L2></div>","<div class=L2></div>","",""),
+  array("<div class=\"E3 E4\"></div>","<div class=\"F3 F4\"></div>","<div class=\"G3 G4\"></div>","<div class=\"I3 I4\"></div>","U","<div class=A4></div>","<div class=L2></div><div class=C4></div>","<div class=L2></div>","<div class=L2></div>","",""),
   
-  array("<div class=\"B3 B4\"></div>","<div class=\"C3 C4\"></div>","<div class=\"D3 D4\"></div>","<div class=\"A3 A4\"></div>","N","<div class=\"F3 F4\"></div>","<div class=L3></div><div class=G4></div>","<div class=L3></div><div class=E4></div>","<div class=L3></div><div class=H4></div>","",""),
+  array("<div class=\"B3 B4\"></div>","<div class=\"C3 C4\"></div>","<div class=\"D3 D4\"></div>","<div class=\"A3 A4\"></div>","N","<div class=\"F3 F4\"></div>","<div class=L3></div><div class=G4></div>","<div class=L3></div><div class=E4></div>","<div class=L3></div><div class=I4></div>","",""),
 
-  array("<div class=\"F3 F4\"></div>","<div class=\"E3 E4\"></div>","<div class=\"H3 H4\"></div>","<div class=\"G3 G4\"></div>","C","<div class=D4></div>","<div class=L4></div><div class=B4></div>","<div class=L4></div>","<div class=L4></div>","","") ,
+  array("<div class=\"F3 F4\"></div>","<div class=\"E3 E4\"></div>","<div class=\"I3 I4\"></div>","<div class=\"G3 G4\"></div>","C","<div class=D4></div>","<div class=L4></div><div class=B4></div>","<div class=L4></div>","<div class=L4></div>","","") ,
 
-  array("<div class=\"C3 C4\"></div>","<div class=\"D3 D4\"></div>","<div class=\"A3 A4\"></div>","<div class=\"B3 B4\"></div>","H","<div class=\"E3 E4\"></div>","<div class=F4></div>","<div class=L5></div>","<div class=L5></div>","","")
+  array("<div class=\"C3 C4\"></div>","<div class=\"D3 D4\"></div>","<div class=\"A3 A4\"></div>","<div class=\"B3 B4\"></div>","H","<div class=\"E3 E4\"></div>","<div class=F4></div><div class=L5></div>","<div class=L5></div>","<div class=L5></div>","","")
   );?>
 <?php
 $courses=array(
+	array("Select Your Courses..","0"),
 	array("IC240","E3"),
-	array("IC241","F3"),
+	array("IC241","B3"),
 	array("IC260","G3"),
-	array("IC250","H3"),
-	//array("CE201","3"),
-	array("IC130","B3"),
+	array("IC250","I3"),
+	array("CE201","A3"),
+	array("IC130","F3"),
 	array("IC136","C3"),
 	array("IC221","D3"),
 	array("IC250(TUE)","L2"),
@@ -36,15 +39,14 @@ $courses=array(
 	array("IC250(THU)","L4"),
 	array("IC130P(THU)","L4"),
 	array("IC222P(THU)","L4"),
-
 	array("HS105","F3"),
-	array("HS263","H3"),
+	array("HS263","I3"),
 	array("HS255","D3"),
 	array("HS502","E3"),
 	array("HS352","G3"),
     array("HS205","D3"),
 	array("HS350","G3"),
-	array("HS343","H3"),
+	array("HS343","I3"),
 	array("HS403","G3"),
 	array("HS471","E3"),
 	array("HS472","F3"),
@@ -52,28 +54,23 @@ $courses=array(
 	array("HS582","A3"),
 	array("HS602","B3"),
 	array("HS620","C3"),
-	array("HS391","H3"),
+	array("HS391","I3"),
 	//array("HS611","3"),
 	//array("HS522","3"),
 	//array("HS621","4"),
 	//array("HS623","4"),
-	
-	
-
 	array("EE311","A3"),
 	array("EE304","C4"),
-	array("EE305","H4"),
-
+	array("EE305","I4"),
 	array("ME206","A3"),
 	array("ME303","B3"),
 	array("ME308","C3"),
 	array("ME305","A4"),
-	
 	array("CS201","B3"),
 	array("CS310","C3"),
 	array("CS303","E3"),
 	array("CS305","G3"),
-	array("CS304","H3"),
+	array("CS304","I3"),
 	array("CS699","F4"),
 	array("CS601","D4"),
 	array("CS609","A4"),
@@ -82,29 +79,27 @@ $courses=array(
     array("CS310(LAB)","L2"),
     array("CS207(2.00-5.00)","L5"),
     array("CS201P","L5"),
-	
 	array("EE608","B4"),
 	array("EE615","G3"),
 	array("EE508","G3"),
 	array("EE509","D3"),
 	array("EE620","C3"),
 	array("EE606","E3"),
-	array("EE609","H3"),
+	array("EE609","I3"),
 	array("EE512","E4"),
 	array("EE518","D3"),
 	array("EE312","L1"),
 	array("EE304","L2"),
 	//array("EE520","3"),
 	//array("EE513","3"),
-	
 	array("ME351","G3"),
 	array("ME356","D4"),
 	array("ME509","E3"),
 	array("ME601","C3"),
-	array("ME602","H3"),
+	array("ME602","I3"),
 	array("ME603","B3"),
-
-	array("ME205(3.00-6.00)","L1"),	array("ME606","D3"),
+	array("ME205(3.00-6.00)","L1"),
+	array("ME606","D3"),
 	array("ME638","D3"),
 	array("ME205(3.00-6.00)","L1"),
 	array("ME311P","L3"),
@@ -112,21 +107,18 @@ $courses=array(
 	array("ME310P","L5"),
 	//array("ME616","3"),
 	//array("ME632","3"),
-	
-	
 	array("EN501","E3"),
 	array("EN502","F3"),
 	array("EN503","C3"),
 	//array("EN604","3"),
 	//array("EN511","3"),
 	//array("EN508","3"),
-
 	array("MA465","D3"),
 	array("MA605","G3"),
-	array("BY505","H3"),
-	array("BY503","H3"),
+	array("BY505","I3"),
+	array("BY503","I3"),
 	array("BY506","C3"),
-	array("BY507","4"),
+	//array("BY507","4"),
 	array("BY508","C3"),
 	//array("BY510","3"),
 	//array("BY511","3"),
@@ -135,72 +127,94 @@ $courses=array(
 	array("PH502","G3"),
 	array("PH506","D3"),
 	array("PH511","G4"),
-	array("PH512","H4"),
+	array("PH512","I4"),
 	array("PH513","F3"),
 	array("PH601","E3"),
 	array("PH613","F3"),
 	//array("PH514","3"),
 	//array("PH611","4"),
 	//array("PH701","4"),
-
 	//array("CY514","3"),
 	//array("CY522","3"),
-	
 	array("CE501","D3"),
 	array("CE601","A3"),
 	
-	array("","")
 	);
 	
-  echo "List of courses<br><select id=clist>";
-	for($i=0;$i<95;$i++)
-	       echo "<option class=".$courses[$i][1]." value=".$courses[$i][0].">".$courses[$i][0]."</option>";
-    echo "</select><br>";
+  
 ?>
-
+	
 <html>
   <head>
   <meta charset="utf-8">
-  
+  <title>Timetable Generator</title>
   <script type="text/javascript" src=js/jquery.min.js></script>
   <script type="text/javascript" src=content/js/bootstrap.min.js></script>
   <link rel="stylesheet" type="text/css" href="content/css/bootstrap.min.css">
   <script type="text/javascript" src=js/send.js></script>
   <link rel="stylesheet" type="text/css" href="design.css">
-  <style type="text/css">#clist {font-size:20px;}
-
-         option {padding:5px;text-align:center;}
-         tr{font-size: 20px;}
+  <style type="text/css">
+  		  #clist {font-size:16px;}
+  		  table{padding: 100px;}
+         option {text-align:center;}
+         tr,td,th{font-size:18px;}
          .jumbotron{background-color:#00F4F4;color:;text-align: center;font-family:monospace;background-image:radial-gradient(#00F4F4,#119191);}
          .small{width:15%;height:15%;}
-         .tt{width:70%;height:70%;}
+         .tt{width:70%;height:70%;margin:100px;}
+         .mdl-navigation{color:black;}
+         #headl{color:white;font-size:22px;}
+         .I3,.I4,.H{background-color: #FF2AAA;}
          </style>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.2.0/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.2.0/material.min.js"></script>
 
   </head>
   <body>
-  <div class=container>
-  	<div class=jumbotron><h2><img src=logo_iit1.png class=small>IInd,IIIrd,IVth Year B.Tech and MTech(Mechanical/Energy)</h2></div>
-  	<ul id=crss>
-  	</ul>
+
+   <!-- Always shows a header, even in smaller screens. -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">IInd,IIIrd,IVth Year B.Tech and MTech(Mechanical/Energy)</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
+      <!-- Navigation. We hide it in small screens. -->
+      <nav class="mdl-navigation mdl-layout-large-screen-only">
+        <!--<a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>-->
+        <?php
+  	echo "<div><!--<div id=headl>List of courses</div>--><select id=clist>";
+	for($i=0;$i<97;$i++)
+	       echo "<option class=".$courses[$i][1]." value=".$courses[$i][0].">".$courses[$i][0]."</option>";
+    echo "</select><br></div>";
+  	?>
+      </nav>
+    </div>
+  </header>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">Batch</span>
+    <nav class="mdl-navigation">
+    <a class="mdl-navigation__link" href="index.php">Home</a>
+      <a class="mdl-navigation__link" href="imp.php">I<sup>st</sup> Year</a>
+      <a class="mdl-navigation__link" href="fourth.php">II<sup>nd</sup>,III<sup>rd</sup>, IV<sup>th</sup> Year and M. Tech.</a>
+      <a class="mdl-navigation__link" href="mscc2016.php">M. Sc.</a>
+      
+    </nav>
   </div>
-<script>
+  <main class="mdl-layout__content">
+    <div class="page-content"><!-- Your content goes here -->
+   <div class=jumbotron><br><h2><img src=logo_iit1.png class=small>B.Tech. and M.Tech.</h2><h3>
+  	II<sup>nd</sup>,III<sup>rd</sup>, IV<sup>th</sup> Year and M.Tech.
+  </h3></div> 		
+  <div class=container>
 
-$('#clist').change(function(){
-	var copt=$('#clist option:selected').val()
-	var slot=$('#clist option:selected').attr('class');
-	$('#crss').append("<li>"+copt+"</li><button>X</button>");
-	$("."+slot).not("option").html(""+copt);
-	//$('#clist option[value="'+copt+'"]').remove();
-});
-//$('.bcls').click(function(){
-//var slot=$(this).attr('class');
-//$("."+slot).not("option").html(" ");
-//});
-</script>
-  </body>
-</html>
-
-<?php
+  	<div id=crss>
+  	</div>
+  	<?php
 
  echo "<table id=tt><tr>";  
   for($i=0;$i<10;$i++)
@@ -216,3 +230,77 @@ $('#clist').change(function(){
       
       }echo "</table>";
 ?>
+<br><br><br><br>
+<b><i>Note:</i></b><ol>
+<li>Slots A3, B3, C3 and D3 are for 3 credit courses that can be used for core subjects.</li>
+<li>Slots A4, B4, C4, D4, E4, F4, G4 and I4 are for 4 credit elective courses, (used for core if needed).</li>
+<li>L1, L2, L3, L4, L5, L6, L7, L8, and L9 are the lab slots.</li>
+<li>CS303 Software Engineering will be conducted from 2pm to 5 pm on Thursday in A5-3 (Conference room).</li>
+<li>CS207 has 1 hour of lecture and will be conducted in slot D4 (Thursday)</li> 
+<li>The classes for I Semester- M.Tech. (Mechanical /Energy) 2016 Batch will be held in North Campus Room B22-1 (Hostel Building).
+</li>
+<li>The classes for EN504/HS540 will be conducted in SC-1</li>
+<li>The Classes for M.Tech. in Biotechnology 2016 Batch will be held in G1-104.</li>
+<li>IC250 Class is only on Monday</li>
+<li>CS207 Theory class is on thursday 1-2 PM</li>
+  </ol>
+  </div>
+    </div>
+  </main>
+</div>
+  <br>
+
+
+
+
+<script>
+
+$('#clist').change(function(){
+	var copt=$('#clist option:selected').val()
+	var slot=$('#clist option:selected').attr('class');
+	if(copt!= "Select") 
+		{
+			$('#crss').append("<span class=\"mdl-chip mdl-chip--deletable\" > <span class=\"mdl-chip__text\">"+copt+"</span><button type=\"button\" class=\"mdl-chip__action \" data="+slot+" contained="+copt+"><i class=\"material-icons\">cancel</i></button></span>");
+	//var x=$("."+slot).not("option").text();
+	//if(text!="") alert(text+'clash Happened');
+	$("."+slot).not("option").html(""+copt);
+
+  var snackbarContainer = document.querySelector('#demo-toast-example');
+  var showToastButton = document.querySelector('#clist');
+	'use strict';
+    var copt=$('#clist option:selected').val()
+    var data = {message: 'Added Course '+copt};
+    snackbarContainer.MaterialSnackbar.showSnackbar(data);
+    $('#clist').val("Select");
+}
+	//$('#clist option[value="'+copt+'"]').remove();
+});
+//$('.bcls').click(function(){
+//var slot=$(this).attr('class');
+//$("."+slot).not("option").html(" ");
+//});
+
+$( document ).on( "click", "button", function() {
+    // jQuery 1.7+
+  var slot=$(this).attr('data');
+  var copt=$(this).attr('contained');
+$("."+slot).not("option").html("");
+//$("span[data="+slot+"]"]).html(" ");
+
+  var snackbarContainer = document.querySelector('#demo-toast-example');
+  var showToastButton = document.querySelector('#clist');
+	'use strict';
+    $(this).parent().remove();
+    var data = {message: 'Removed Course '+copt};
+    snackbarContainer.MaterialSnackbar.showSnackbar(data);
+
+});
+</script>
+
+
+<div id="demo-toast-example" class="mdl-js-snackbar mdl-snackbar">
+  <div class="mdl-snackbar__text"></div>
+  <button class="mdl-snackbar__action" type="button"></button>
+</div>
+</body>
+</html>
--- imp.php
+++ imp.php
@@ -8,15 +8,76 @@ $ftime = array
   (
   array("<div class=A>IC110</div>","<div class=B>IC140</div>","<div class=C>IC150</div>","<div class=D>IC160</div>","","","",""),
   
-  array(" ","<div class=L6>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L6>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L6>IC150P<br>IC160P<br>IC140<br>IC101P</div>"," "," "," ","<div class=\"hs_1 H\">HS106</div>"),
+  array(" ","<div class=L6>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L6>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L6>IC150P<br>IC160P<br>IC140<br>IC101P</div>"," "," "," ","<div class=\"hs_elec H\">HS106</div>"),
   
   array("<div class=B>IC140</div>","<div class=L7>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L7>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L7>IC150P<br>IC160P<br>IC140<br>IC101P</div>"," ","<div class=C>IC150</div>","<div class=D>IC160</div>","<div class=A>IC110</div>"),
 
-  array(" ","<div class=L8>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L8>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L8>IC150P<br>IC160P<br>IC140<br>IC101P</div>"," ","","<div class=\"hs_1 H\">HS106</div>"," ") ,
+  array(" ","<div class=L8>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L8>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L8>IC150P<br>IC160P<br>IC140<br>IC101P</div>"," ","","<div class=\"hs_elec H\">HS106</div>"," ") ,
 
   array("<div class=C>IC150</div>","<div class=L9>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L9>IC150P<br>IC160P<br>IC140<br>IC101P</div>","<div class=L9>IC150P<br>IC160P<br>IC140<br>IC101P</div>"," ","<div class=D>IC160</div>","<div class=A>IC110</div>","<div class=B>IC140</div>"),
   );
- echo "<table><tr>";  
+ ?>
+
+<html>
+  <head>
+  <meta charset="utf-8">
+  <title>Timetable Generator</title>
+  <script type="text/javascript" src=js/jquery.min.js></script>
+  <link rel="stylesheet" type="text/css" href="design.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.2.0/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.2.0/material.min.js"></script>
+<link rel="stylesheet" type="text/css" href="design.css">
+<style type="text/css">
+    
+  .jumbotron{background-color:#00F4F4;color:;text-align: center;font-family:monospace;background-image:radial-gradient(#00F4F4,#119191); padding: 20px;}
+         .small{width:15%;height:15%;}
+         table{margin:auto;padding:auto;}
+         input{margin-right:300px;}
+</style>
+
+  </head>
+  <body>
+  
+  
+ <!-- Always shows a header, even in smaller screens. -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">I<sup>st</sup> Year</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
+      <!-- Navigation. We hide it in small screens. -->
+      <!--<nav class="mdl-navigation mdl-layout-large-screen-only">
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+        <a class="mdl-navigation__link" href="">Link</a>
+      </nav>-->
+    </div>
+  </header>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">Batch</span>
+    <nav class="mdl-navigation">
+    <a class="mdl-navigation__link" href="index.php">Home</a>
+      <a class="mdl-navigation__link" href="imp.php">I<sup>st</sup> Year</a>
+      <a class="mdl-navigation__link" href="fourth.php">II<sup>nd</sup>,III<sup>rd</sup>, IV<sup>th</sup> Year and M. Tech.</a>
+      <a class="mdl-navigation__link" href="mscc2016.php">M. Sc.</a>
+      
+    </nav>
+  </div>
+  <main class="mdl-layout__content">
+    <div class="page-content"><!-- Your content goes here -->
+    
+    <div class=jumbotron><br><br><h2><img src=logo_iit1.png class=small> B. Tech.</h2>
+    I<sup>st</sup> Year
+    </div>
+
+<br>
+<br><br>
+  <?php
+echo "<table><tr>";  
   for($i=0;$i<9;$i++)
     echo "<th>".$time[$i]."</th>";
    echo "</tr>";
@@ -28,31 +89,36 @@ $ftime = array
       }
     echo "</tr>";
       
-      }echo "</table>";?>
+      }echo "</table>";
+?>
+  
+  <div class="mdl-grid">
+  <div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet"></div>
+  <div class="mdl-cell mdl-cell--4-col mdl-cell--6-col-tablet">
+  <b>Electives:</b><label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="hselec">
+  <input type="checkbox" id="hselec" class="mdl-checkbox__input hselec">
+  <span class="mdl-checkbox__label">HS106</span>
+</label>
+</div>
+  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone"></div>
+</div>
 
-<html>
-  <head>
-  <meta charset="utf-8">
-  <script type="text/javascript" src=js/jquery.min.js></script>
-  <link rel="stylesheet" type="text/css" href="design.css">
-  </head>
-  <body>
-  <label><strong>Electives</strong> </label>: <input type=checkbox id='hs1' class=hs1>HS106 <br>
-  <label><strong>Year</strong><label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: 
-  <a ><input type= radio name='year' id=i checked=checked>I'st Year</a>
-  <a href=second.php><input type= radio name='year' id=ii>II'nd Year</a>
-  <a href=third.php><input type= radio name='year' id=iii>III'rd Year</a>
-  <a href=fourth.php><input type= radio name='year' id=iv >IV'th Year</a><br>
+</div>
+  </main>
+</div>
+<br><br>
+
+  
   <script>
 
 $(document).ready(function(){
-  $('.hs_1').hide();
+  $('.hs_elec').hide();
 });
-$('.hs1').change(function(){
+$('.hselec').change(function(){
   if($(this).prop('checked'))
-    $('.hs_1').show()  // checked
+    $('.hs_elec').show()  // checked
 else
-    $('.hs_1').hide();  // unchecked
+    $('.hs_elec').hide();  // unchecked
 });
 
 </script>
